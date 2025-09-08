f35a3fd8056b4e8477aaaeb6cf47d2700e67b881
manishbeth18 <manish.beth@gmail.com>
2025-09-08 12:22:45 +0530

    Add files via upload

---
+++ InDecExample.java
@@ -0,0 +1,12 @@
+public class InDecExample {
+    public static void main(String[] args){
+        int x =10;
+        System.out.println("Original:" + x);
+        System.out.println("Post-increment:"+(x++));
+        System.out.println("After post increment:" +x);
+        System.out.println("Pre-increment:"+(++x));
+        System.out.println("Post-decrement :"+(x--));
+        System.out.println("Pre-decrement:" +(--x));
+    }
+
+}
